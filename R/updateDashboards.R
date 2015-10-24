fc9f1cfc42c912fdc68c3127741753029939602e
Shivkumar Shivaji <sshivaji@gmail.com>
2015-10-24 13:55:37 -0700

    Update updateDashboards.R

--- R/updateDashboards.R
+++ R/updateDashboards.R
@@ -44,7 +44,7 @@ updateDashboards <- function(dashboards = NULL){
             message("Dashboard threw an error updating:")
             message(cond)
           }
-          warnng=function(cond) {
+          warning=function(cond) {
             message("Dashboard threw a warning updating:")
             message(cond)
           }
